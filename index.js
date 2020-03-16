c0710406d94c5574b4a8c1d75d430f71cb84a032
Jeet Mandaliya <45892659+sereneinserenade@users.noreply.github.com>
2020-03-16 17:58:19 +0100

    Revert " just beautified"
    This reverts commit a66137e0df99ec29882f66999f4df2547a02d2eb.

--- index.js
+++ index.js
@@ -11,37 +11,37 @@ const githubToken = process.env.GITHUB_TOKEN
 const githubEvent = require(process.env.GITHUB_EVENT_PATH)
 const config = YAML.parse(fs.readFileSync(configPath, 'utf8'))
 
-async function exec() {
-    try {
-        const result = await new Action({
-            githubEvent,
-            argv: parseArgs(),
-            config,
-            githubToken,
-        }).execute()
+async function exec () {
+  try {
+    const result = await new Action({
+      githubEvent,
+      argv: parseArgs(),
+      config,
+      githubToken,
+    }).execute()
 
-        if (result) {
-            console.log(`Created issues: ${result.issues}`)
+    if (result) {
+      console.log(`Created issues: ${result.issues}`)
 
-            // Produce a well-formed JSON array of all newly created issue keys
-            core.setOutput("issues", JSON.stringify(result.issues, null, 4))
+      // Produce a well-formed JSON array of all newly created issue keys
+      core.setOutput("issues", JSON.stringify(result.issues, null, 4))
 
-            return
-        }
-
-        process.exit(0)
-    } catch (error) {
-        console.error(error)
-        process.exit(1)
+      return
     }
+
+    process.exit(0)
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
 }
 
-function parseArgs() {
-    return {
-        project: core.getInput('project'),
-        issuetype: core.getInput('issuetype'),
-        description: core.getInput('description')
-    }
+function parseArgs () {
+  return {
+    project: core.getInput('project'),
+    issuetype: core.getInput('issuetype'),
+    description: core.getInput('description')
+  }
 }
 
 exec()

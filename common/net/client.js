1e8083f446c58f1248844f7ba1c1143e14b5da5a
Jeet Mandaliya <45892659+sereneinserenade@users.noreply.github.com>
2020-03-16 17:53:02 +0100

    just beautified

--- common/net/client.js
+++ common/net/client.js
@@ -1,24 +1,24 @@
 const fetch = require('node-fetch')
 
 module.exports = serviceName => async (state, apiMethod = 'unknown') => {
-  const response = await fetch(state.req.url, state.req)
+    const response = await fetch(state.req.url, state.req)
 
-  state.res = {
-    headers: response.headers.raw(),
-    status: response.status,
-  }
+    state.res = {
+        headers: response.headers.raw(),
+        status: response.status,
+    }
 
-  state.res.body = await response.text()
+    state.res.body = await response.text()
 
-  const isJSON = (response.headers.get('content-type') || '').includes('application/json')
+    const isJSON = (response.headers.get('content-type') || '').includes('application/json')
 
-  if (isJSON && state.res.body) {
-    state.res.body = JSON.parse(state.res.body)
-  }
+    if (isJSON && state.res.body) {
+        state.res.body = JSON.parse(state.res.body)
+    }
 
-  if (!response.ok) {
-    throw new Error(response.statusText)
-  }
+    if (!response.ok) {
+        throw new Error(response.statusText)
+    }
 
-  return state
+    return state
 }

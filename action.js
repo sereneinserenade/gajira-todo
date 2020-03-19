28f87bce0c399f5e0bcd622277c01084883d550a
Jeet Mandaliya <45892659+sereneinserenade@users.noreply.github.com>
2020-03-19 14:02:44 +0100

    logging des

--- action.js
+++ action.js
@@ -129,6 +129,7 @@ module.exports = class {
       console.log(c)
       console.log('__________________')
 
+      const newDes = 'Created by' + c.committer.name
       this.argv.description = `Created by: ${c.committer.name}`
 
       return getMatches(res, rx, 1)
